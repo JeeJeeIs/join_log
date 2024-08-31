73668accf9c97feaa6b3731309540e4581791f9f
JeeJeeIs <69713393+JeeJeeIs@users.noreply.github.com>
2024-08-31 14:57:11 +0300

    f

---
+++ output/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'JeeJee'
+
+-- What to run
+client_scripts {
+    'Client.net.dll'
+}
+
+server_scripts {
+    'Server.net.dll'
+}
